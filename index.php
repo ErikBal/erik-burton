db753a2e068f82f037229fe29b237a9738887fe5
Erik Balonier <erikibali@gmail.com>
2015-03-12 18:45:02 -0500

    basic html structur almost completed

---
+++ index.php
@@ -0,0 +1,167 @@
+<html>
+	<head>
+		<link href="css/stylesheet.css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<section id="top">
+				<div id="wrap">
+					<nav>
+						<ul>
+							<li>
+								<img src="//akamai-ssl.burton.com/2015/images/flags/Burton_US-default.gif">
+							</li>
+							<li>
+								<a href="#">My Account *</a>   
+							</li>
+							<li>
+								<a href="#">Stores</a>  
+							</li>
+							<li>
+								<a href="#">Gift Cards</a>  
+							</li>
+						</ul>
+					</nav>
+					<div>
+						<p>
+							FREE STANDARD SHIPPING ON ORDERS OVER $75
+						</p>
+					</div>
+				</div>
+			</section>
+			<section id="bottom">
+				<div id="wrap2">
+					<nav>
+						<ul>
+							<li>
+								<a href="#">MEN</a>
+							</li>
+							<li>
+								<a href="#">WOMEN</a>
+							</li>
+							<li>
+								<a href="#">YOUTH</a>
+							</li>
+							<li>
+								<a href="#">BAGS</a>
+							</li>
+							<li>
+								<a href="#">CAMPING</a>
+							</li>
+							<li>
+								<a href="#">SALE</a>
+							</li>
+						</ul>
+					</nav>
+					<div>
+						<a href="#">
+							<span></span>
+						</a>		
+					</div>
+					<nav>
+						<div>
+
+						</div>
+						<ul>
+							<li>
+								<a href="#">13 THINGS</a>
+							</li>
+							<li>
+								<a href="#">BURTON GIRLS</a>
+							</li>
+							<li>
+								<a href="#">TEAM</a>
+							</li>
+							<li>
+								<a href="#">ABOUT US</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+			</header>
+			<main>
+				<section>
+					<div>
+					</div>
+					<nav>
+						<ul>
+							<li>
+							</li>
+							<li>
+							</li>
+							<li>
+							</li>
+						</ul>
+					</nav>
+				</section>
+			</main>
+			<section>
+				<div>
+				</div>
+				<div>
+				</div>
+			</section>
+			<section>
+				<div>
+				</div>
+			</section>
+			<section>
+				<nav>
+					<ul>
+						<li>
+						</li>
+					</ul>
+				</nav>
+			</section>
+			<section>
+				<div>
+					
+				</div>
+			</section>
+			<footer>
+				<div>
+					<section>
+						<article>
+						</article>
+						<nav>
+							<ul>
+								<li>
+									
+								</li>
+								<li>
+									
+								</li>
+								<li>
+									
+								</li>
+								<li>
+									
+								</li>
+							</ul>
+						</nav>
+						<div>
+							<nav>
+								<ul>
+									<li>
+
+									</li>
+									<li>
+
+									</li>
+									<li>
+										
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</section>
+					<section>
+					</section>
+					<section>
+					</section>
+					<section>
+				</div>
+			</footer>
+		</body>
+</html>
